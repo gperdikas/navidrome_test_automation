d442ae1901b96b5cf415a1854e66d758a150f788
Qun12est <qualitynestqa@gmail.com>
2025-11-20 22:18:12 +0200

    header ui tests

--- tests/logged-in/ui/header.spec.ts
+++ tests/logged-in/ui/header.spec.ts
@@ -3,37 +3,54 @@ import { HeaderPage } from '../../../pages/header-page';
 
 test('Admin is able to see header', {tag: ['@regression', '@smoke', '@header']}, async ({page}) => {
     const headerPage = new HeaderPage(page);
-    
     await headerPage.goto();
 
     await expect(headerPage.header).toBeVisible();
 });
 
-/*
-test('Admin is able to see the menu button', {tag: ['@regression', '@smoke', '@header']}, async ({page}) => {
 
+test('Admin is able to see the menu button', {tag: ['@regression', '@smoke', '@header']}, async ({page}) => {
+    const headerPage = new HeaderPage(page);
+    await headerPage.goto();
+
+    await expect(headerPage.menuButton).toBeVisible();
 });
 
 test('Admin is able to see the tile of the open tab', {tag: ['@regression', '@smoke', '@header']}, async ({page}) => {
+    const headerPage = new HeaderPage(page);
+    await headerPage.goto();
 
+    await expect(headerPage.title).toBeVisible();
 });
 
 test('Admin is able to see the refresh button', {tag: ['@regression', '@smoke', '@header']}, async ({page}) => {
+    const headerPage = new HeaderPage(page);
+    await headerPage.goto();
 
+    await expect(headerPage.refreshButton).toBeVisible();
 });
 
 test('Admin is able to see the Now Playing button', {tag: ['@regression', '@smoke', '@header']}, async ({page}) => {
+    const headerPage = new HeaderPage(page);
+    await headerPage.goto();
 
+    await expect(headerPage.nowPlayingButton).toBeVisible();
 });
 
 test('Admin is able to see the Activity button', {tag: ['@regression', '@smoke', '@header']}, async ({page}) => {
+    const headerPage = new HeaderPage(page);
+    await headerPage.goto();
 
+    await expect(headerPage.activityButton).toBeVisible();
 });
 
 test('Admin is able to see the Settings button', {tag: ['@regression', '@smoke', '@header']}, async ({page}) => {
+    const headerPage = new HeaderPage(page);
+    await headerPage.goto();
 
+    await expect(headerPage.settingsButton).toBeVisible();
 });
-*/
+
 test('When menu button is set on ON position, menu is visible', {tag: ['@regression', '@smoke', '@header']}, async ({page}) => {
     const headerPage = new HeaderPage(page);
     
