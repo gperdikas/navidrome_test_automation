4cdd5ca76ff1b927d396231fb223fe2ae87c7908
Qun12est <georgekperdikas@gmail.com>
2026-09-02 18:41:10 +0300

    Remove dead comments and whitespace

--- tests/admin-logged-in/admin-header.spec.ts
+++ tests/admin-logged-in/admin-header.spec.ts
@@ -1,35 +1,37 @@
 import { test, expect } from '@playwright/test';
 import { HeaderPage } from '../../pages/header-page';
 
-test('Admin is able to see the Activity button', {tag: ['@loggedin', '@ui', '@header', '@admin']}, async ({page}) => {
-    const headerPage = new HeaderPage(page);
-    await headerPage.goto();
+test.describe('Admin is able to see Headers', () => {
+    test('Admin is able to see the Activity button', {tag: ['@loggedin', '@ui', '@header', '@admin']}, async ({page}) => {
+        const headerPage = new HeaderPage(page);
+        await headerPage.goto();
 
-    await expect(headerPage.activityButton).toBeVisible();
-});
+        await expect(headerPage.activityButton).toBeVisible();
+    });
 
-test('Admin clicks Activity button and popup opens', {tag: ['@loggedin', '@ui', '@header', '@admin']}, async ({page}) => {
-    const headerPage = new HeaderPage(page);
-    await headerPage.goto();
-    await headerPage.clickActivity();
+    test('Admin clicks Activity button and popup opens', {tag: ['@loggedin', '@ui', '@header', '@admin']}, async ({page}) => {
+        const headerPage = new HeaderPage(page);
+        await headerPage.goto();
+        await headerPage.clickActivity();
 
-    await expect(headerPage.activityPopover).toBeVisible();
-    await expect(headerPage.uptimeInfoBox).toBeVisible();
-    await expect(headerPage.foldersScannedInfoBox).toBeVisible();
-    await expect(headerPage.scanInfoBox).toBeVisible();
-});
+        await expect(headerPage.activityPopover).toBeVisible();
+        await expect(headerPage.uptimeInfoBox).toBeVisible();
+        await expect(headerPage.foldersScannedInfoBox).toBeVisible();
+        await expect(headerPage.scanInfoBox).toBeVisible();
+    });
 
-test('Admin is able to see the Now Playing button', {tag: ['@loggedin', '@ui', '@header', '@admin']}, async ({page}) => {
-    const headerPage = new HeaderPage(page);
-    await headerPage.goto();
+    test('Admin is able to see the Now Playing button', {tag: ['@loggedin', '@ui', '@header', '@admin']}, async ({page}) => {
+        const headerPage = new HeaderPage(page);
+        await headerPage.goto();
 
-    await expect(headerPage.nowPlayingButton).toBeVisible();
-});
+        await expect(headerPage.nowPlayingButton).toBeVisible();
+    });
 
-test('User clicks Now Playing button and popup opens', {tag: ['@loggedin', '@ui', '@header', '@admin']}, async ({page}) => {
-    const headerPage = new HeaderPage(page);
-    await headerPage.goto();
-    await headerPage.clickNowPlaying();
+    test('User clicks Now Playing button and popup opens', {tag: ['@loggedin', '@ui', '@header', '@admin']}, async ({page}) => {
+        const headerPage = new HeaderPage(page);
+        await headerPage.goto();
+        await headerPage.clickNowPlaying();
 
-    await expect(headerPage.nowPlayingInfoBox).toBeVisible();
+        await expect(headerPage.nowPlayingInfoBox).toBeVisible();
+    });
 });
